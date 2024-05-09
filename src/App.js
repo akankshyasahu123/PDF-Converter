880c0dcb19c7a7bb4c5b2519812869761500774a
Akankshya Sahu <142734670+akankshyasahu123@users.noreply.github.com>
2024-05-09 14:58:50 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TextEditor from './TextEditor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextEditor/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from "react";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.snow.css";
+// import DOMPurify from "dompurify";
+// import { jsPDF } from "jspdf";
+// import Logo from './Logo.png';
+// import './App.css';
+
+// const App = () => {
+//   const [editorText, setEditorText] = useState("");
+//   const editorRef = useRef(null);
+
+//   useEffect(() => {
+//     if (editorRef.current) {
+//       editorRef.current.focus();
+//     }
+//   }, []);
+
+//   const handleChange = (_content, _delta, _source, editor) => {
+//     const sanitizedHtml = DOMPurify.sanitize(editor.getHTML());
+//     setEditorText(sanitizedHtml);
+//   };
+
+//   const generatePdf = () => {
+//     const pdf = new jsPDF();
+//     const pw = pdf.internal.pageSize.getWidth();
+//     const ph = pdf.internal.pageSize.getHeight();
+//     const lineHeight = 12; 
+//     let minPages = 2; 
+
+//     const plainText = editorText.replace(/<[^>]+>/g, '');
+//     const paragraphs = plainText.split('\n'); 
+
+//     let yPosition = 40; 
+//     let pageCount = 1;
+
+//     const generateFooter = (pageNumber) => {
+//       pdf.setFontSize(10);
+//       pdf.text(`Page ${pageNumber} of ${minPages}`, pw / 2, ph - 10, { align: "center" });
+//       pdf.text(`Copyright © ${new Date().getFullYear()}`, pw / 2, ph - 20, { align: "center" });
+//     };
+
+//     pdf.addImage(Logo, "PNG", 10, 10, 20, 20);
+//     pdf.setFontSize(12);
+//     generateFooter(pageCount);
+
+//     paragraphs.forEach((paragraph, index) => {
+//       const lines = pdf.splitTextToSize(paragraph, pw - 40); 
+//       lines.forEach((line, lineIndex) => {
+//         const lineLength = pdf.getStringUnitWidth(line) * pdf.internal.getFontSize() / pdf.internal.scaleFactor;
+//         const startX = index === 0 && lineIndex === 0 ? pw - lineLength - 20 : 20;
+//         pdf.text(line, startX, yPosition);
+//         yPosition += lineHeight; 
+//         if (yPosition > ph - 40) {
+//           pdf.addPage();
+//           pageCount++;
+//           minPages++;
+//           pdf.addImage(Logo, "PNG", 10, 10, 20, 20);
+//           pdf.setFontSize(12);
+//           generateFooter(pageCount);
+//           yPosition = 40;
+//         }
+//       });
+//     });
+
+//     for (let i = pageCount; i < minPages; i++) {
+//       pdf.addPage();
+//       pdf.addImage(Logo, "PNG", 10, 10, 20, 20);
+//       pdf.setFontSize(12);
+
+//       generateFooter(i + 1);
+//     }
+
+//     pdf.autoPrint();
+//     pdf.save('new.pdf');
+//   };
+
+//   return (
+//     <div>
+//       <h2>Text Editor</h2>
+//       <ReactQuill ref={editorRef} value={editorText} onChange={handleChange} style={{ margin: '30px', direction: 'ltr' }} />
+//       <button className="btn" onClick={generatePdf}>Print PDF</button>
+//     </div>
+//   );
+// };
+
+// export default App;
---
+++ src/TextEditor.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import DOMPurify from "dompurify";
+import { jsPDF } from "jspdf";
+import Logo from './Logo.png';
+
+const TextEditor = () => {
+  const [editorText, setEditorText] = useState("");
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.focus();
+    }
+  }, []);
+
+  const handleChange = (_content, _delta, _source, editor) => {
+    const sanitizedHtml = DOMPurify.sanitize(editor.getHTML());
+    setEditorText(sanitizedHtml);
+  };
+
+  const generatePdf = () => {
+    const pdf = new jsPDF();
+    const pw = pdf.internal.pageSize.getWidth();
+    const ph = pdf.internal.pageSize.getHeight();
+    const lineHeight = 12; 
+    let minPages = 2; 
+
+    const plainText = editorText.replace(/<[^>]+>/g, '');
+    const paragraphs = plainText.split('\n'); 
+
+    let yPosition = 40; 
+    let pageCount = 1;
+
+    const generateFooter = (pageNumber) => {
+      pdf.setFontSize(10);
+      pdf.text(`Page ${pageNumber} of ${minPages}`, pw / 2, ph - 10, { align: "center" });
+      pdf.text(`Copyright © ${new Date().getFullYear()}`, pw / 2, ph - 20, { align: "center" });
+    };
+
+    pdf.addImage(Logo, "PNG", 10, 10, 20, 20);
+    pdf.setFontSize(12);
+    generateFooter(pageCount);
+
+    paragraphs.forEach((paragraph, index) => {
+      const lines = pdf.splitTextToSize(paragraph, pw - 40); 
+      lines.forEach((line, lineIndex) => {
+        const lineLength = pdf.getStringUnitWidth(line) * pdf.internal.getFontSize() / pdf.internal.scaleFactor;
+        const startX = index === 0 && lineIndex === 0 ? pw - lineLength - 20 : 20;
+        pdf.text(line, startX, yPosition);
+        yPosition += lineHeight; 
+        if (yPosition > ph - 40) {
+          pdf.addPage();
+          pageCount++;
+          minPages++;
+          pdf.addImage(Logo, "PNG", 10, 10, 20, 20);
+          pdf.setFontSize(12);
+          generateFooter(pageCount);
+          yPosition = 40;
+        }
+      });
+    });
+
+    for (let i = pageCount; i < minPages; i++) {
+      pdf.addPage();
+      pdf.addImage(Logo, "PNG", 10, 10, 20, 20);
+      pdf.setFontSize(12);
+      generateFooter(i + 1);
+    }
+
+    pdf.autoPrint();
+    pdf.save('new.pdf');
+  };
+
+  return (
+    <div>
+      <h2 style={{margin:'30px'}}>Text Editor</h2>
+      <ReactQuill ref={editorRef} value={editorText} onChange={handleChange} style={{ margin: '30px', direction: 'ltr' }} />
+      <button  style={{margin: '30px',padding: '10px',border: 'none',color: 'white',backgroundColor:'#04AA6D'}}onClick={generatePdf}>
+       Print PDF</button>
+    </div>
+  );
+};
+
+export default TextEditor;
